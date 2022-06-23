bf8252ab34788edf476a223b320f980ecad01175
Saturno <samuelmathias033@gmail.com>
2022-06-23 17:20:33 -0300

    @samuelsaturn [Task: start feed]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { FeedComponent } from './feed/feed.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    FeedComponent
   ],
   imports: [
     BrowserModule,
